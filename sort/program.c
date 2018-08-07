997786ba07b95778a1370c79ce9c574ebe452b19
gahan9 <gahansaraiya@gmail.com>
2018-08-07 09:26:45 +0530

    added function for swap and generating random array

--- sort/program.c
+++ sort/program.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define TEST_NUM 1000
+int* generate_array(int max_element){
+    // generate array of n elements
+    static int array[100];
+    for(int i=0; i < max_element; i++){
+        array[i] = rand();
+    }
+    return array;
+}
+
+void swap(int *one, int *two)
+{
+    // swap function to swap elements by location/address
+    int temp = *one;
+    *one = *two;
+    *two = temp;
+}
+
 int bubble(int* array){
     int temp, i, j;
     size_t arr_size = sizeof(array);
@@ -20,8 +38,8 @@ int bubble(int* array){
 }
 
 int main(){
-    int num = 100000;
-
+    int i, num = 100000;
+    int *arr_ptr;
     /*
     int *ptr, count = 0, i;
     ptr = (int*) malloc(num * sizeof(int));
@@ -39,6 +57,12 @@ int main(){
     }
     fclose (fp);
     */
+    arr_ptr = generate_array(TEST_NUM);  // store generated array of random TEST_NUM numbers
+    
+    // // loop for debug to check which elements are generated randomly
+    // for(i=0; i<TEST_NUM; i++){
+    //     printf( "*(arr_ptr + %d) : %d\n", i, *(arr_ptr + i));
+    // }
     // res = bubble(&arr);
     return 0;
 }
