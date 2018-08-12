907fc8bf88be0d57d2824e8ece7c5d937c099d75
gahan9 <gahansaraiya@gmail.com>
2018-08-13 00:15:42 +0530

    removed unnecessary function/data and added quick sort function and analysis of it

--- sort/program.c
+++ sort/program.c
@@ -4,7 +4,7 @@
 #include<stdlib.h>
 #include<time.h>
 
-#define TEST_NUM 1000
+#define TEST_NUM 10000
 
 int* generate_array(int max_element){
     // generate array of n elements
@@ -46,30 +46,6 @@ char** split_string(char* str) {
     return splits;
 }
 
-char* readline() {
-    // read file line -- copied from hackerrank challange template solution
-    size_t alloc_length = 1024;
-    size_t data_length = 0;
-    char* data = malloc(alloc_length);
-
-    while (true) {
-        char* cursor = data + data_length;
-        char* line = fgets(cursor, alloc_length - data_length, stdin);
-        if (!line) { break; }
-        data_length += strlen(cursor);
-        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
-        size_t new_length = alloc_length << 1;
-        data = realloc(data, new_length);
-        if (!data) { break; }
-        alloc_length = new_length;
-    }
-    if (data[data_length - 1] == '\n') {
-        data[data_length - 1] = '\0';
-    }
-    data = realloc(data, data_length);
-    return data;
-}
-
 void read_file_input() {
     // under development function to read inputs from file
     int ptr[TEST_NUM], count = 0, i, ar_count;
@@ -77,42 +53,21 @@ void read_file_input() {
     FILE *fp = fopen("file.in", "r");
 
     char in = fgetc(fp);
-    ar_count = (int) (in - '0');
-    printf("arr cnt : %d", ar_count);
-    char* ar_count_str = readline();
+    // ar_count = (int) (in - '0');
+    printf("\narr\n");
     // fgets(c, 1024, fp);
-    int cnt = 0;
-    while (1) {
-        printf("cnt: %d", cnt);
-        cnt++;
-        if (cnt >20)
-            break;
-        if (fgets(c,150, fp) == NULL) break;
-        i++;
-        printf("%3d: %s", i, c);
-    }
-    printf("%d\n",i);
-    // while((in = fgetc(fp)) != EOF)
-    //     printf("in: %c", in);
-
-    char** ar_temp = split_string(&in);
-    printf("---> %s", ar_temp[0]);
-    for (int ar_itr = 0; ar_itr < 15; ar_itr++) {
-        char* ar_item_endptr;
-        char* ar_item_str = ar_temp[ar_itr];
-        int ar_item = strtol(ar_item_str, &ar_item_endptr, 10);
-        printf("-->%d. %d", ar_itr, ar_item);
-        if (ar_item_endptr == ar_item_str || *ar_item_endptr != '\0') { exit(EXIT_FAILURE); }
-
-        c[ar_itr] = ar_item;
-    }
-
-    // while (in != EOF){
-    //     // ptr[count] = in;
-    //     // fgetc(fp);
-    //     in = readline();
-    //     count += 1;
-    // }
+     while (in != EOF){
+        if ((int) (in -'0') == -16){
+            printf("\nspace\n");
+        }
+        else{
+            printf("%c - %d\n",in,  (int) (in - '0'));
+        }
+         // ptr[count] = in;
+         // fgetc(fp);
+         in = fgetc(fp);
+         count += 1;
+     }
     // fscanf(fp,"%[^\n]", c);
     // printf("Data from the file:\n%s", c);
     // fscanf(fp,"%[^\n]", c);
@@ -164,7 +119,7 @@ int* insertion(int* array, int no_of_elements){
 
 int* selection(int* array, int no_of_elements){
     // Insertion sort algorithm
-//    printf("\n--------------------Insertion sort--------------------\n");
+//    printf("\n--------------------Selection sort--------------------\n");
     for (register int i = 0; i < no_of_elements-1; i++){  // iterate up to second last element
         int min = i;  // set current index as minimum
         for (register int j = i+1; j < no_of_elements; j++){  // iterate over all elements of certain range
@@ -175,6 +130,36 @@ int* selection(int* array, int no_of_elements){
             swap(&array[i], &array[min]);  // swap minimum element scanned/iterated so far
     }
     return array;
+}
+
+int partition (int* array, int start, int end)
+{
+    int pivot = array[end];    // select pivot
+    int i = (start - 1);  // get index of smaller element
+
+    for (int j=start; j <= end- 1; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[end]);
+    return (i + 1);
+}
+
+int* quick(int *array, int start, int end)
+{
+    if (start < end)
+    {
+        int partition_index = partition(array, start, end);
+
+        // Separately sort elements before
+        // partition and after partition
+        quick(array, start, partition_index - 1);
+        quick(array, partition_index + 1, end);
+    }
 }
 
 int main(){
@@ -209,7 +194,15 @@ int main(){
     t = clock() - t;
     cpu_time_consumption = ((double) (t)) / CLOCKS_PER_SEC;
     printf("\nSelection sort took %f amount of time", cpu_time_consumption);
+    // quick sort
+    t = clock();
+    arr_ptr = generate_array(TEST_NUM);
+    quick(arr_ptr, 0, TEST_NUM-1);
+    t = clock() - t;
+    cpu_time_consumption = ((double) (t)) / CLOCKS_PER_SEC;
+    printf("\nQuick sort took %f amount of time", cpu_time_consumption);
 
+    // read_file_input();
     // display_array(res, TEST_NUM);
     printf("\n\n-*-*-*-*-*-*-*-*-*-END OF PROGRAM*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
     return 0;
