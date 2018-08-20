b41b96d0b46aa0d1181f15694f40276e3290857e
gahan9 <gahansaraiya@gmail.com>
2018-08-20 23:26:49 +0530

    code refactor

--- sort/plot_graph.py
+++ sort/plot_graph.py
@@ -11,9 +11,8 @@ STORAGE_DIR = os.path.join(".", "analysis")
 os.makedirs(STORAGE_DIR, exist_ok=True)
 
 df_sample = pd.read_csv('https://raw.githubusercontent.com/gahan9/DSA_lab/master/sort/analysis_test.csv')
-
 color = ["#ff7c00", "#a91f78", "#0432ff", "#00a643", "#a6f900", "#fffb00", "#ffd300", "#c8e000"]
-# df_sample = df_sample.loc[df_sample['log10(DATA_SET)'] != 4.69]
+
 ALGORITHMS = [
     'Bubble_sort_(Iterative)',
     'Heap_sort_(Recursive)',
@@ -22,21 +21,11 @@ ALGORITHMS = [
     'Quick_sort_(Recursive)',
     'Selection_sort_(Iterative)'
 ]
+
 def plot_graph(algo_name):
     data = []
     df_filtered = df_sample.loc[df_sample['ALGORITHM'] == algo_name]
     data_set_type = ['ascending', 'descending', 'random', 'same']
-    # data_set_type = ['ascending', 'descending', 'random']
-    # fig = plotly.tools.make_subplots(rows=6, cols=2,
-    #                                  specs=[[{}, {'rowspan': 2}],
-    #                                         [{}, None],
-    #                                         [{'rowspan': 2}, None],
-    #                                         [None, None],
-    #                                         [None, None],
-    #                                         [{}, {}]],
-    #                                  print_grid=False)
-    # trace_cnt = 1
-    # for algo in ALGORITHMS:
     cnt = 0
     for i in data_set_type:
         data_algo_type = df_filtered.loc[df_filtered['DATA_SET_TYPE'] == i]
@@ -46,9 +35,6 @@ def plot_graph(algo_name):
         trace = go.Scatter(x=x, y=y, text=[], hovertext=[], name=i, marker=dict(color=color[cnt]))
         data += [trace]
         cnt += 1
-        #     fig.append_trace(trace, trace_cnt, 1)
-        #     fig.append_trace(trace, trace_cnt, 2)
-        # trace_cnt += 1
     layout = go.Layout(title=algo_name, barmode='group',
                        xaxis={'title': 'data set (considering log base 10)', 'tickformat': ',d'},
                        yaxis={'title': 'time (ms)', 'tickformat': ',d'})
@@ -62,4 +48,4 @@ def plot_graph(algo_name):
 
 
 if __name__ == "__main__":
-    list(map(plot_graph, ALGORITHMS))
+    list(map(plot_graph, set(df_sample['ALGORITHM'])))
