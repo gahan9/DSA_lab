b5b3232efd35ce7e35a405b8d01fc6909fa2054a
gahan9 <gahansaraiya@gmail.com>
2018-10-08 13:10:55 +0530

    added practical list

---
+++ README.md
@@ -0,0 +1,13 @@
+PRACTICAL_LIST
+==============
+Sorting
+
+B+ tree
+
+Binomial Heap
+
+Krushkal's Algorithm with disjoint set data structure
+
+Strassen's Algorithm for matrix multiplication
+
+Recursive Exponential with Divide and Conquer
---
+++ greedy/strassians_matrix_multiplication.c
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------------------------------------
+// Author: Gahan Saraiya
+// GiT: http://github.com/gahan9/
+// StackOverflow: https://stackoverflow.com/users/story/7664524
+// Website: http://gahan9.github.io/
+// --------------------------------------------------------------------------------------------
+// Implementing Strassen's's Matrix Multiplication Algorithm with Divide and Conquer
+#include <stdio.h>
+#include <stdlib.h>
+
+int ONE[MAX][MAX];
+int TWO[MAX][MAX];
+
+int main(){
+    return 1;
+}
