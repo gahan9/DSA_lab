07a23fe0c9e11459c12f125c98662e6d498ab261
gahan9 <gahansaraiya@gmail.com>
2018-10-15 23:28:48 +0530

    matrix implementation changed to pointer to pointer

--- 5.strassen/strassen_s_matrix_multiplication.c
+++ 5.strassen/strassen_s_matrix_multiplication.c
@@ -13,32 +13,73 @@
 
 #define MAX 2048
 
-int ONE[MAX][MAX];
-int TWO[MAX][MAX];
-int RESULT[MAX][MAX];
+//int ONE[MAX][MAX];
+//int ONE[MAX][MAX] = {{1,0,0,0}, {1,2,3,4}, {0,1,0,1}, {2,2,5,10}};
+//int TWO[MAX][MAX] = {{1,0,3,8}, {2,5,5,4}, {8,1,8,3}, {9,9,5,10}};
+//int TWO[MAX][MAX];
+//int RESULT[MAX][MAX];
+int **ONE, **TWO, **RESULT;
 
-void strassen_multiply(int matrix1[MAX][MAX], int matrix2[MAX][MAX], int operational_size, int offset_i, int offset_j){
+int ** matrix_add(int **matrix1, int **matrix2){
+    int **ans;
+    for (int i=0; i < 2; i++){
+        for (int j=0; j < 2; j++){
+
+        }
+    }
+    return ans;
+}
+
+
+void conquer(int matrix[MAX][MAX], int operational_size){
+    RESULT;
+}
+
+
+void strassen_multiply(int **matrix1, int **matrix2, int operational_size, int offset_i, int offset_j){
     if (operational_size == 2){
         // Apply strassen multiplication
         int mul1, mul2, mul3, mul4, mul5, mul6, mul7;
-        mul1 = (matrix1[offset_i+1][offset_i+1] + matrix1[offset_i+2][offset_i+2]) * (matrix2[offset_i+1][offset_i+1] + matrix2[offset_i+2][offset_i+2]);
-        mul2 = (matrix1[offset_i+2][offset_i+1] + matrix1[offset_i+2][offset_i+2]) * (matrix2[offset_i+1][offset_i+1]);
-        mul3 = (matrix1[offset_i+1][offset_i+1]) * (matrix2[offset_i+1][offset_i+2] - matrix2[offset_i+2][offset_i+2]);
-        mul4 = (matrix1[offset_i+2][offset_i+2]) * (matrix2[offset_i+2][offset_i+1] - matrix2[offset_i+1][offset_i+1]);
-        mul5 = (matrix1[offset_i+1][offset_i+1] + matrix1[offset_i+1][offset_i+2]) * (matrix2[offset_i+2][offset_i+2]);
-        mul6 = (matrix1[offset_i+2][offset_i+1] - matrix1[offset_i+1][offset_i+1]) * (matrix2[offset_i+1][offset_i+1] + matrix2[offset_i+1][offset_i+2]);
-        mul7 = (matrix1[offset_i+1][offset_i+2] - matrix1[offset_i+2][offset_i+2]) * (matrix2[offset_i+2][offset_i+1] + matrix2[offset_i+2][offset_i+2]);
+        int a, b, c, d, e, f, g, h;
+//        mul1 = (matrix1[offset_i][offset_i] + matrix1[offset_i+1][offset_i+1]) * (matrix2[offset_i][offset_i] + matrix2[offset_i+1][offset_i+1]);
+//        mul2 = (matrix1[offset_i+1][offset_i] + matrix1[offset_i+1][offset_i+1]) * (matrix2[offset_i][offset_i]);
+//        mul3 = (matrix1[offset_i][offset_i]) * (matrix2[offset_i][offset_i+1] - matrix2[offset_i+1][offset_i+1]);
+//        mul4 = (matrix1[offset_i+1][offset_i+1]) * (matrix2[offset_i+1][offset_i] - matrix2[offset_i][offset_i]);
+//        mul5 = (matrix1[offset_i][offset_i] + matrix1[offset_i][offset_i+1]) * (matrix2[offset_i+1][offset_i+1]);
+//        mul6 = (matrix1[offset_i+1][offset_i] - matrix1[offset_i][offset_i]) * (matrix2[offset_i][offset_i] + matrix2[offset_i][offset_i+1]);
+//        mul7 = (matrix1[offset_i][offset_i+1] - matrix1[offset_i+1][offset_i+1]) * (matrix2[offset_i+1][offset_i] + matrix2[offset_i+1][offset_i+1]);
+//        RESULT[offset_i][offset_j] = mul1 + mul4 - mul5 + mul7;
+//        RESULT[offset_i][offset_j+1] = mul3 + mul5;
+//        RESULT[offset_i+1][offset_j] = mul2 + mul4;
+//        RESULT[offset_i+1][offset_j+1] = mul1 - mul2 + mul3 + mul6;
+        a = matrix1[offset_i][offset_j];
+        b = matrix1[offset_i][offset_j+1];
+        c = matrix1[offset_i+1][offset_j];
+        d = matrix1[offset_i+1][offset_j+1];
+        e = matrix2[offset_i][offset_j];
+        f = matrix2[offset_i][offset_j+1];
+        g = matrix2[offset_i+1][offset_j];
+        h = matrix2[offset_i+1][offset_j+1];
 
-        RESULT[offset_i+1][offset_j+1] = mul1 + mul4 - mul5 + mul7;
-        RESULT[offset_i+1][offset_j+2] = mul3 + mul5;
-        RESULT[offset_i+2][offset_j+1] = mul2 + mul4;
-        RESULT[offset_i+2][offset_j+2] = mul1 - mul2 + mul3 + mul6;
+        mul1 = a * (f-h);
+        mul2 = (a + b) * h;
+        mul3 = (c + d) * e;
+        mul4 = d * (g - e);
+        mul5 = (a + d) * (e + h);
+        mul6 = (b - d) * (g + h);
+        mul7 = (a - c) * (e + f);
+
+        RESULT[offset_i][offset_j] = mul5 + mul4 - mul2 + mul6;
+        RESULT[offset_i][offset_j+1] = mul1 + mul2;
+        RESULT[offset_i+1][offset_j] = mul3 + mul4;
+        RESULT[offset_i+1][offset_j+1] = mul1 + mul5 - mul3 - mul7;
     }
     else{
         strassen_multiply(matrix1, matrix2, operational_size/2, 0, 0);
         strassen_multiply(matrix1, matrix2, operational_size/2, operational_size/2, 0);
         strassen_multiply(matrix1, matrix2, operational_size/2, 0, operational_size/2);
         strassen_multiply(matrix1, matrix2, operational_size/2, operational_size/2, operational_size/2);
+//        conquer(RESULT, operational_size/2);
     }
 }
 
@@ -47,19 +88,32 @@ int main(int argc, char *argv[]){
     if (argv[1])
         array_size = atoi(argv[1]);
     printf("array size: %d\n", array_size);
-    for(int i=0; i<array_size; i++){
+//    ONE[4][4] = {{1,0,0,0}, {1,2,3,4}, {0,1,0,1}, {2,2,5,10}};
+    int i;
+    //------------------ Memory Allocation
+    int **ONE = (int **)malloc(array_size * sizeof(int *));
+    int **TWO = (int **)malloc(array_size * sizeof(int *));
+    int **RESULT = (int **)malloc(array_size * sizeof(int *));
+    for (i=0; i<array_size; i++) {
+        ONE[i] = (int *) malloc(array_size * sizeof(int));
+        TWO[i] = (int *) malloc(array_size * sizeof(int));
+        RESULT[i] = (int *) malloc(array_size * sizeof(int));
+    }
+
+    for(i=0; i<array_size; i++){
         for(int j=0; j<array_size; j++) {
             ONE[i][j] = 1;
             TWO[i][j] = 1;
+            RESULT[i][j] = 0;
         }
     }
     printf("Printing ONE\n");
-    show_2d_array(ONE, array_size);
+    display_2d_array(ONE, array_size);
     printf("Printing TWO\n");
-    show_2d_array(TWO, array_size);
+    display_2d_array(TWO, array_size);
 
     strassen_multiply(ONE, TWO, array_size, 0, 0);
     printf("RESULT----\n");
-    show_2d_array(RESULT, array_size);
+    display_2d_array(RESULT, array_size);
     return 1;
 }
